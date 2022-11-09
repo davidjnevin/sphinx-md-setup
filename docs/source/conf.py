0620ae41d01cae95ff1a5727d02aee08e202a46c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-11-09 17:12:32 +0100

    Clean up unneeded comments in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,6 @@ release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.autosectionlabel",
@@ -53,12 +50,10 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "furo"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# Add any paths that contain custom static files relative to this directory.
+# They are copied after the builtin static files.
+# Existing files arr overwriten.
+
 html_static_path = ["_static"]
